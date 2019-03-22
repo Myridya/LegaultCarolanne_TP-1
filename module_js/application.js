2596094a1aed71f7fefc683825d875d86d8e5033
Carolanne <myridya@hotmail.com>
2019-03-22 12:54:08 -0400

    Commit 9 : Ajout de animation Jello

--- module_js/application.js
+++ module_js/application.js
@@ -3,9 +3,9 @@ import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
 import {Introduction} from './Introduction.js' // 
 import {AnimLettre} from './AnimLettre.js' //
 
-
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
 let elmHeader = document.querySelector('.header')
+let elmContent = document.querySelector('.content')
 let intro = new Introduction(contenuIntro, elmHeader, animationLettre)
 
 // debutQuestionnaire()
@@ -16,11 +16,6 @@ function animationLettre()
 	console.log('debut animation lettre')
 	const lesLettres = 'Veille-technologique' // Elle reste toujours les même puisqu'elles ne sont jamais modifiées par l'animation
  	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim)	
-}
-
-function AnimationLigne()
-{
-	let nomAnimLigne = new AnimLigne();
 }
 
 
